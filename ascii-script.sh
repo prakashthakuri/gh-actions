c091f98e5544890f1c31d810e8f2f2fa0ce06daa
Prakash Thakuri <prakashthakuri1994@gmail.com>
2024-09-10 06:59:30 -0500

    Update ascii-script.sh

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 sudo apt-get install cowsay -y
-cowsay -f lion "Run for cover, I am lion" >> lion.txt
+cowsay -f tux "Run for cover, I am lion" >> lion.txt
 grep -i "lion" lion.txt
 cat lion.txt
